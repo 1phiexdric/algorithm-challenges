5d3e3fe42a9564cbc96961179310de852ca98e99
Friedrich Ruiz <ruizlopezfriedrichjavier@gmail.com>
2025-09-30 21:59:07 -0400

    añadiendo una nueva forma de bubbleSort

--- Ordenamiento numerico/bubbleSort.js
+++ Ordenamiento numerico/bubbleSort.js
@@ -1,3 +1,4 @@
+#sigue evaluando elementos ya ordenados
 const bubbleSort = (arr)=>{
     for(let i = 0; i < arr.length; i++){
         for(let j = 0; j < arr.length - 1; j++){
@@ -11,5 +12,20 @@ const bubbleSort = (arr)=>{
     }
     return arr
 }
+#no evalúa elementos ordenados
+function bubbleSort2(arr){
+  for(let i = 0; i <= arr.length;i++){
+     for(let j = 0; j<= arr.length-i;j++){
+        if(arr[j] > arr[j+1]){
+             let temp = arr[j]
+             arr[j] = arr[j+1]
+             arr[j+1] = temp
+          }
+       }
+    }
+  return arr
+}
+
+
 
 console.log(bubbleSort([5, 3, 8, 4, 2]))
